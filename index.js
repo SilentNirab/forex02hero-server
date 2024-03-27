971c776ba53b95dc77a793be3bed3f4787d2dea9
Md Nirab <nabilnirab255@gmail.com>
2024-03-28 00:30:04 +0600

    first commit

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 
+
 //ForexHero
 //M8bg8F28DFPCXYV5
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://ForexHero:M8bg8F28DFPCXYV5@cluster0.ezxu64p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,15 +31,136 @@ async function run() {
     await client.connect();
 
     const userCollection = client.db("Forex02Hero").collection("users")
+    const postCollection = client.db("Forex02Hero").collection("posts")
+    const newsCollection = client.db("Forex02Hero").collection("news")
 
-    app.post("/users", async(req, res)=>{
-        const user = req.body;
-        console.log("user", user);
-        const result = await userCollection.insertOne(user);
-        console.log(result);
-        res.send(result);
+    // user API
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
     })
 
+    app.get('/users/client/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let client = false;
+      if (user) {
+        client = user?.role === 'client';
+      }
+      res.send({ client });
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+    app.patch('/users/client/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'client'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // posts API
+    app.get('/posts', async (req, res) => {
+      const result = await postCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await postCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.post('/posts', async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post)
+      res.send(result)
+    })
+
+    // app.put('/posts/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updateposts = req.body;
+    //   const booking = {
+    //     $set: {
+    //       product_image: updateposts.product_image,
+    //       product_name: updateposts.product_name,
+    //       description: updateposts.description,
+    //     }
+    //   }
+    //   const result = await postCollection.updateOne(filter, booking, options);
+    //   res.send(result);
+    // })
+    app.delete('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await postCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    //news api
+    app.get('/news', async (req, res) => {
+      const result = await newsCollection.find().toArray()
+      res.send(result)
+    })
+    app.post('/news', async (req, res) => {
+      const news = req.body;
+      const result = await newsCollection.insertOne(news)
+      res.send(result)
+    })
+
+    app.delete('/news/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await newsCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
@@ -50,7 +172,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 
 app.get("/", (req, res) => {
